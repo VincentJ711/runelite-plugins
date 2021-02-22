e0d219ec7a42c92595731629e784ea3bd0ae2142
VincentJ711 <vincentj711@gmail.com>
2021-02-22 01:32:38 -0800

    add border to tabs

--- src/main/kotlin/com/itemcarts/ui/MainNavRow.kt
+++ src/main/kotlin/com/itemcarts/ui/MainNavRow.kt
@@ -8,17 +8,20 @@ import javax.inject.Provider
 import javax.swing.Box
 import javax.swing.BoxLayout
 import javax.swing.JPanel
+import javax.swing.border.EmptyBorder
+import javax.swing.border.MatteBorder
 
 class MainNavRow : JPanel(), Destroyable {
   @Inject
   private lateinit var viewManager: Provider<ViewManager>
+
   private val cartsBtn = LabelButton(
     LabelButtonOpts(
       text = "carts",
       bgColor = ColorScheme.DARKER_GRAY_COLOR,
       bgHoverColor = ColorScheme.DARKER_GRAY_HOVER_COLOR,
       textColor = TEXT_PRIMARY,
-      onClick = { viewManager.get().goToCartsView() }
+      onClick = { toCarts() }
     )
   )
 
@@ -28,9 +31,12 @@ class MainNavRow : JPanel(), Destroyable {
       bgColor = ColorScheme.DARKER_GRAY_COLOR,
       bgHoverColor = ColorScheme.DARKER_GRAY_HOVER_COLOR,
       textColor = TEXT_PRIMARY,
-      onClick = { viewManager.get().goToSummaryView() }
+      onClick = { toSummary() }
     )
   )
+
+  private val selectedBorder = MatteBorder(0, 0, 1, 0, ColorScheme.BRAND_ORANGE)
+  private val noBorder = EmptyBorder(0, 0, 1, 0)
 
   init {
     layout = BoxLayout(this, BoxLayout.X_AXIS)
@@ -42,6 +48,7 @@ class MainNavRow : JPanel(), Destroyable {
     cartsBtn.minimumSize = Dimension(72, 32)
     cartsBtn.preferredSize = Dimension(72, 32)
     cartsBtn.maximumSize = Dimension(72, 32)
+    cartsBtn.border = selectedBorder
 
     summaryBtn.minimumSize = Dimension(72, 32)
     summaryBtn.preferredSize = Dimension(72, 32)
@@ -50,6 +57,18 @@ class MainNavRow : JPanel(), Destroyable {
     add(cartsBtn)
     add(Box.createHorizontalStrut(8))
     add(summaryBtn)
+  }
+
+  private fun toCarts() {
+    cartsBtn.border = selectedBorder
+    summaryBtn.border = noBorder
+    viewManager.get().goToCartsView()
+  }
+
+  private fun toSummary() {
+    cartsBtn.border = noBorder
+    summaryBtn.border = selectedBorder
+    viewManager.get().goToSummaryView()
   }
 
   override fun onBeforeDestroy() {
